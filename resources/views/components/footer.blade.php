ad39fd1b4c0662fcaf37d4d4c89a27a280a66101
Alessandro Pasquinelli <pasquiale89@hotmail.it>
2023-05-30 23:06:02 +0200

    Navabar, footer, masthead, bulding degli asset

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<div class="footer-basic">
+    <footer>
+        <div class="social"><a href="#"><i class="fa-brands fa-instagram"></i></a><a href="#"><i class="fa-brands fa-snapchat"></i></a><a href="#"><i class="fa-brands fa-twitter"></i></a><a href="#"><i class="fa-brands fa-facebook"></i></a></div>
+        <ul class="list-inline">
+            <li class="list-inline-item"><a href="#">Home</a></li>
+            <li class="list-inline-item"><a href="#">Services</a></li>
+            <li class="list-inline-item"><a href="#">About</a></li>
+            <li class="list-inline-item"><a href="#">Terms</a></li>
+            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p class="copyright">Buy.IT © 2023</p>
+    </footer>
+</div>
